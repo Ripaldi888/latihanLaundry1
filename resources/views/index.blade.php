09f498df8ac152c9e5cf172dce06f8a6bd236b64
Ripaldi888 <muhripaldi07@gmail.com>
2025-02-17 19:09:56 +0700

    perubahan css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
     <div class="header">
         <div class="logo">
             <img src="{{ asset('assets/img/logo.png') }}" alt="">
-            <p>Laundry Smk Citra Negara</p>
+            <p class="mt-3">Laundry Smk Citra Negara</p>
         </div>
         <nav class="navbar">
             <ul>
@@ -30,8 +30,9 @@
     <div class="content">
         <h1>Lihat Pesanan</h1>
     </div>
-
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="1" d="M0,256L40,256C80,256,160,256,240,224C320,192,400,128,480,122.7C560,117,640,171,720,192C800,213,880,203,960,165.3C1040,128,1120,64,1200,42.7C1280,21,1360,43,1400,53.3L1440,64L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
 <section class="search">
+    
     <form class="pesanan-container" action="{{ route('search.noHp') }}" role="search" method="POST">
         @csrf
         <div class="pesanan-row">
@@ -44,7 +45,7 @@
             <div class="pesanan-input-wrapper">
                 <input type="text" name="no_hp" class="tracking-input" placeholder="Nomor handphone" required>
             </div>
-            <button type="submit" id="cariPesanan" class="pesanan-button">Search</button>
+            <button type="submit" id="cariPesanan" class="pesanan-button mt-2">Search</button>
         </div>
     </form>
     @if (isset($datas))
@@ -78,7 +79,7 @@
     
 
 
-
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="1" d="M0,288L30,261.3C60,235,120,181,180,160C240,139,300,149,360,165.3C420,181,480,203,540,213.3C600,224,660,224,720,197.3C780,171,840,117,900,80C960,43,1020,21,1080,32C1140,43,1200,85,1260,133.3C1320,181,1380,235,1410,261.3L1440,288L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
 
 
 <footer>
